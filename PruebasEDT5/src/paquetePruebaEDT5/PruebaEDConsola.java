447aa6958249fb6d2885ebba31b37214520e2283
curro <fjarandabarba@gmail.com>
2023-05-10 19:06:12 +0200

    commit 3

--- PruebasEDT5/src/paquetePruebaEDT5/PruebaEDConsola.java
+++ PruebasEDT5/src/paquetePruebaEDT5/PruebaEDConsola.java
@@ -6,5 +6,6 @@ public class PruebaEDConsola {
 		System.out.println("Hola, prueba 1");
 		System.out.println("Commit 2 de prueba");
 	}
-
+	/* https://github.com/FJAB1992/EdPruebasT5.git
+		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 */
 }
