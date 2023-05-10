b4d94cdd3ec5875b45377a804bdc9bc5bce436d7
curro <fjarandabarba@gmail.com>
2023-05-10 19:15:19 +0200

    pusheo 2

--- PruebasEDT5/src/paquetePruebaEDT5/PruebaEDConsola.java
+++ PruebasEDT5/src/paquetePruebaEDT5/PruebaEDConsola.java
@@ -5,6 +5,7 @@ public class PruebaEDConsola {
 	public static void main(String[] args) {
 		System.out.println("Hola, prueba 1");
 		System.out.println("Commit 2 de prueba");
+		System.out.println("Commit 3 espera");
 	}
 	/* https://github.com/FJAB1992/EdPruebasT5.git
 		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 */
